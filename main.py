d5647729b84f12f5540012b2093982c1f47d4d8a
KreozotGit <230250275+KreozotGit@users.noreply.github.com>
2026-03-28 21:47:46 +0300

    Create

---
+++ main.py
@@ -0,0 +1,10 @@
+def symma_nechetnih(m_number):
+    spisok = [int(i) for i in range(1, m_number + 1)]
+    s = 0
+    for i in range(len(spisok)):
+        if spisok[i] % 2 == 1:
+            s += spisok[i]
+    return s
+
+data = int(input('Пожалуйста, введите верхнюю границу количества чисел в списке: '))
+print(f'Сумма всех отрицательных чисел в заданном диапазоне: {symma_nechetnih(data)}')
